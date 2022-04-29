7be24da7885adb53282d746454e9d80243576b62
Keerti Chaudhary <17keerti@gmail.com>
2022-04-29 04:34:26 -0700

    add initial prompts for user choice

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,15 +18,89 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-db.query('SELECT * FROM employee', function (err, results) {
+function userOptions() {
+  inquirer.prompt([{
+            type: "list",
+            name: "userChoices",
+            message: "What would you like to do ?",
+            choices: ["View All Employees", 
+                      "View All Employees By Department", 
+                      "View All Employees By Manager", 
+                      "Add Emplyee", "Remove Employee", 
+                      "Update Employee Role", 
+                      "Update Employee Manager", 
+                      "View All Roles", 
+                      "Add Role", 
+                      "Remove Role", 
+                      "View All Departments", 
+                      "Add Department", 
+                      "Remove Department", 
+                      "Quit"]
 
-  if(results){
-    results.forEach(function(employee){
-    console.log(employee)
+  }]).then(function(answers) {
+
+    switch (answer.userChoices){
+    case "View All Employees":
+      viewAllEmployees();
+      break;
+
+    case "View All Employees By Department":
+      viewByDepartment();
+      break;
+
+    case "View All Employees By Manager":
+      viewByManager();
+      break;
+
+    case "Add Emplyee":
+      addEmployee();
+      break;
+
+    case "Remove Employee":
+      removeEmployee();
+      break;
+      
+    case "Update Employee Role":
+      updateRole();
+      break;
+      
+    case "Update Employee Manager":
+      updateManager();
+      break;
+
+    case "View All Roles":
+      viewAllRoles();
+      break;
+
+    case "Add Role":
+      addRole();
+      break;
+
+    case "Remove Role":
+      removeRole();
+      break;
+
+    case "View All Departments":
+      viewAllDepartments();
+      break;
+      
+    case "Add Department":
+      addDepartment();
+      break;
+      
+    case "Remove Department":
+      removeDepartment();
+      break;
+      
+    case "Quit":
+      quit();
+      break; 
+    }
+
   })
-  }
-  
-});
+}
+
+userOptions();
 
 app.use((req, res) => {
   res.status(404).end();
